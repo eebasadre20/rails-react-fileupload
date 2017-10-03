ff408573c80dbb21f3add81b0a09f70abf2feac5
Edsil Basadre <eebasadre20@gmail.com>
2017-10-03 15:20:29 +0800

    fix(): Imagemagick issue regarding the styles.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,11 +1,11 @@
 class Picture < ApplicationRecord
   belongs_to :imageable, polymorphic: true, optional: true
-  has_attached_file :avatar, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
+  has_attached_file :image, 
+    default_style: :medium,
+    styles: { medium: "300x300>", thumb: "100x100>" },
+    url: ':s3_domain_url',
+    path: '/:class/:attachment/:id_partition/:style/:filename'
 
-  validates_attachment_content_type :image , 
-    :content_type => ['image/jpeg', 'image/jpg', 'image/png','image/x-png', 'image/gif']
+  validates_attachment_content_type :image, 
+    :content_type => ['image/jpeg','image/png','image/x-png', 'image/gif']
 end
